0275f9ad6954eb245ce4f1e5ed2cc973a4a7fd88
kux-kodes <119114087+kux-kodes@users.noreply.github.com>
2024-02-01 13:35:17 +0200

    added day and night themes

--- app/src/main/java/com/example/journal_app/ThemePages.kt
+++ app/src/main/java/com/example/journal_app/ThemePages.kt
@@ -1,36 +1,38 @@
 package com.example.journal_app
 
+import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.DividerDefaults.color
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+    val lightColors = lightColorScheme(
+        primary= Color(153, 220, 255),
+        secondary= Color(209, 246, 85),
+        tertiary=  Color(156, 200, 7),
+    )
 
-@Preview(showBackground=true)
+    val nightColours = darkColorScheme(
+       primary= Color(139, 85, 246),
+        secondary = Color(85,19,216),
+        tertiary = Color(29,27,34)
+    )
 @Composable
-fun DayTheme(){
-    var themeColour1: Color= Color(0, 212, 212, 255)
-    var themeColour2: Color= Color(157, 216, 88, 255)
-    val dayColours= Brush.verticalGradient(listOf(themeColour1,themeColour2 ))
-    Column(modifier= Modifier
-        .fillMaxSize()
-        .background(dayColours)){
-    }
-
+fun AppTheme(
+    darkTheme: Boolean= isSystemInDarkTheme(),
+    content: @Composable ()-> Unit,
+){
+    val colors= if (darkTheme) nightColours else lightColors
+    MaterialTheme(
+    colorScheme= colors,
+        content= content
+    )
 }
-@Preview(showBackground=true)
-@Composable
-fun NightTheme(){
-    var themeColour3: Color= Color(139,85,246)
-    var themeColour4: Color= Color(85, 19, 216)
-    val nightColours= Brush.verticalGradient(listOf(themeColour3,themeColour4))
-    Column(modifier= Modifier
-        .fillMaxSize()
-        .background(nightColours)){
-    }
-}
-
