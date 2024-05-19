886d3ce00d2544d004c3c37fc1ff4711b8abc3e8
kux-kodes <119114087+kux-kodes@users.noreply.github.com>
2024-05-19 17:30:45 +0200

    added part of the foundation

---
+++ app/src/main/java/com/example/journal_app/data/local/NoteDatabase.kt
@@ -0,0 +1,28 @@
+package com.example.journal_app.data.local
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import androidx.room.TypeConverter
+import androidx.room.TypeConverters
+import java.util.Date
+object DataConverters{
+    @TypeConverter
+    fun fromTimeStamp(value:Long?):Date?{
+        return if(value==null) null else Date(value)
+    }
+    @TypeConverters
+    fun dateToTimestamp (date:Date?):Long{
+        return date.time?
+    }
+}
+@Database(
+    entities= [NoteModel::class],
+    version= 1,
+    exportSchema = true
+)
+
+@TypeConverters(DateConverters::class){
+    abstract class NoteDatabase: RoomDatabase(){
+        abstraction fun dao(): NoteDao
+    }
+}
---
+++ app/src/main/java/com/example/journal_app/data/local/NoteModel.kt
@@ -0,0 +1,20 @@
+package com.example.journal_app.data.local
+
+import android.os.Parcelable
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import kotlinx.parcelize.Parcelize
+import java.util.Date
+
+@Parcelize
+@Entity(tableName = "note_table")
+data class NoteModel(
+    @PrimaryKey(autoGenerate = true) val id: Int,
+    @ColumnInfo(name= "tile") val title: String,
+    @ColumnInfo(name= "note") val note:String,
+    @ColumnInfo(name= "created_at : ") val createdAt: Date= Date(System.currentTimeMillis()),
+    @ColumnInfo(name= "updated_at : ") val updatedAt: Date= Date(System.currentTimeMillis())
+) : Parcelable {
+    constructor(title: String, note: String) : this(0, title, note)
+}
