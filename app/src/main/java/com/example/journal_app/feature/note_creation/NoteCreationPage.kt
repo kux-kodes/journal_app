1d561e6cc76038c6f295ac53a3c5bf7f54fcf240
kux-kodes <119114087+kux-kodes@users.noreply.github.com>
2024-07-18 20:22:24 +0200

    progress with NCP view

--- app/src/main/java/com/example/journal_app/feature/note_creation/NoteCreationPage.kt
+++ app/src/main/java/com/example/journal_app/feature/note_creation/NoteCreationPage.kt
@@ -1,10 +1,13 @@
 package com.example.journal_app.feature.note_creation
 
+import android.view.ViewTreeObserver
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
@@ -15,6 +18,7 @@ import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
 import com.example.journal_app.R
 import com.example.journal_app.data.local.NoteModel
+import com.example.journal_app.feature.note_creation.components.NoteCreationAppBar
 import com.example.journal_app.feature.note_creation.view_model.NoteCreationPageBaseVM
 import com.example.journal_app.feature.note_creation.view_model.NoteCreationPageVM
 import kotlinx.coroutines.launch
@@ -23,50 +27,79 @@ const val TAG: String = "NoteCreationPage"
 @ExperimentalMaterial3Api
 @Composable
 fun NoteCreationPage(navHostController: NavHostController,
-                     viewModel: NoteCreationPageBaseVM = hiltViewModel<NoteCreationPageVM>()){
+                     viewModel: NoteCreationPageBaseVM = hiltViewModel<NoteCreationPageVM>()) {
     val titleTextField = "${stringResource(R.string.title_textField)}-${TAG}}"
     val bodyTextField = "${stringResource(R.string.body_textField)}-${TAG}}"
     val titleInput = stringResource(R.string.title_textField_input)
     val bodyInput = stringResource(R.string.body_textField_input)
     val length = viewModel.descriptionText.value.length
     val scope = rememberCoroutineScope()
-    val requiredDialogState= remember{mutableStateOf(false)}
-    val cancelDialogState= remember{mutableStateOf(false)}
-    val snackBarHostState= remember{SnackbarHostState()}
-    fun addNote(){
-        if(viewModel.titleText.value.isEmpty()|| viewModel.descriptionText.value.isEmpty()){
+    val requiredDialogState = remember { mutableStateOf(false) }
+    val cancelDialogState = remember { mutableStateOf(false) }
+    val snackBarHostState = remember { SnackbarHostState() }
+    fun addNote() {
+        if (viewModel.titleText.value.isEmpty() || viewModel.descriptionText.value.isEmpty()) {
             requiredDialogState.value = true
             //TODO: why are we setting the value to true?
-        } else{
+        } else {
             scope.launch {
-                viewModel.addNote(NoteModel(
-                    title = viewModel.titleText.value ,
-                    note = viewModel.descriptionText.value
-                )).onSuccess { navHostController.popBackStack()
-                snackBarHostState.showSnackbar(
-                    message = "note successfully added" ,
-                    withDismissAction = true
-                )}
-                    .onFailure {  snackBarHostState.showSnackbar(
-                        message = it.message ?: "",
+                viewModel.addNote(
+                    NoteModel(
+                        title = viewModel.titleText.value,
+                        note = viewModel.descriptionText.value
+                    )
+                ).onSuccess {
+                    navHostController.popBackStack()
+                    snackBarHostState.showSnackbar(
+                        message = "note successfully added",
                         withDismissAction = true
-                    )}
+                    )
+                }
+                    .onFailure {
+                        snackBarHostState.showSnackbar(
+                            message = it.message ?: "",
+                            withDismissAction = true
+                        )
+                    }
                 //TODO: Explain the above code piece
             }
         }
     }
+
     val appBarState = rememberTopAppBarState()
-//TODO: Resolve top bar error and explain why it happened
     val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior(appBarState)
-    val remerberedScrollBehavior = remember {scrollBehavior}
+    val rememberedScrollBehavior = remember { scrollBehavior }
     val view = LocalView.current
-    val keyboardHeight = remember {mutableStateOf(0.dp)}
-    val viewTreeObserver = remember {view.viewTreeObserver}
+    //TODO: What does this do?
+    val keyboardHeight = remember { mutableStateOf(0.dp) }
+    val viewTreeObserver = remember { view.viewTreeObserver }
+    //TODO: What's a tree observer? why is it relevant?
     val onGlobalLayoutListener = remember {
-        val rect = android.graphics.Rect().apply {
-            view.getWindowVisibleDisplayFrame(this)
+        ViewTreeObserver.OnGlobalLayoutListener {
+            val rect = android.graphics.Rect().apply {
+                view.getWindowVisibleDisplayFrame(this)
+            }
+            //TODO: What is Rect?
+            //TODO: What is a global layout listener? how is it relevant?
+            val keyboardHeightNew = view.rootView.height - rect.bottom
+            if (keyboardHeightNew.dp != keyboardHeight.value) {
+                keyboardHeight.value = keyboardHeightNew.dp
+            }
+            //TODO: Explain how this piece of code works
         }
-//TODO: fix errors
+    }
+    DisposableEffect(view) {
+        viewTreeObserver.addOnGlobalLayoutListener(onGlobalLayoutListener)
+        onDispose {
+            viewTreeObserver.removeOnGlobalLayoutListener(onGlobalLayoutListener)
+        }
+    }
+    Scaffold(
+        topBar = {
+            NoteCreationAppBar()
+        }
+    ) {
+
     }
 }
 
---
+++ app/src/main/java/com/example/journal_app/feature/note_creation/components/AppBar.kt
@@ -0,0 +1,15 @@
+package com.example.journal_app.feature.note_creation.components
+
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+
+@OptIn(ExperimentalMaterial3Api::class)
+//TODO: how do you use OptIn?
+//TODO: What does ::class mean? why is it necessary?
+@Composable
+fun NoteCreationAppBar(
+    descriptionTextLength: Int
+    onBackPressed: () -> Unit
+    //TODO: How does onBackPressed work?
+
+){}
